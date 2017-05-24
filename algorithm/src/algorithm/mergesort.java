1c2ca13f380eb1efa800140645bed99e1cd41ff1
sea5sky2 <sea5sky2@gmail.com>
2017-05-24 10:30:30 +0900

    test

--- algorithm/src/algorithm/mergesort.java
+++ algorithm/src/algorithm/mergesort.java
@@ -3,8 +3,23 @@ package algorithm;
 public class mergesort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("머지소트");
+		int[] A = { 8, 31, 48, 73, 3, 65, 20, 29, 11, 15 };
+
+		// 원본 배열 출력
+		for (int a : A)
+			System.out.print(a + " ");
+		System.out.println();
+
+		MergeSort(A);
+
+		// 정렬 배열 출력
+		for (int a : A)
+			System.out.print(a + " ");
+		System.out.println();
+	}
+	
+	public static void MergeSort(int[] arr) {
+		
 	}
 
 }
